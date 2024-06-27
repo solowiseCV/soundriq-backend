c1d269a80bc58464503c8cc641aac832169a58cb
Yosinan <ymymym4634@gmail.com>
2024-06-27 13:00:51 +0300

    included artist registration route

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import status from "./routes/status";
 import userRouter from "./routes/userRoutes";
 import fileRouter from "./routes/fileRoutes";
+import artistRouter from "./routes/artistRoutes";
 import errorHandler from "./middlewares/errorHandler";
 import setupSwagger from "./swagger/swagger";
 
@@ -12,10 +13,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// Middleware to parse URL-encoded data
+// Middleware to parse URL-encoded data 
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/user", userRouter);
+app.use("/artist", artistRouter);
 app.use("/", status);
 app.use("/files", fileRouter);
 
