620d23e8031c205152e653c4a31141bb8c551c20
Yosinan <ymymym4634@gmail.com>
2024-07-15 19:34:05 +0300

    modified: prisma schema to handle reset token and blacklisted token

---
+++ prisma/migrations/20240715163109_edit_user_table/migration.sql
@@ -0,0 +1,83 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ArtistProfile" (
+    "id" TEXT NOT NULL,
+    "artistName" TEXT,
+    "dateOfBirth" TEXT,
+    "profilePhoto" TEXT,
+    "bannerImage" TEXT,
+    "signatureSound" TEXT,
+    "countryOfOrigin" TEXT,
+    "bio" TEXT,
+    "musicGenre" TEXT,
+    "appleMusicAddress" TEXT,
+    "spotifyAddress" TEXT,
+    "soundCloudAddress" TEXT,
+    "youtubeAddress" TEXT,
+    "instagramAddress" TEXT,
+    "tiktokAddress" TEXT,
+    "manager" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT,
+
+    CONSTRAINT "ArtistProfile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "File" (
+    "id" TEXT NOT NULL,
+    "filename" TEXT NOT NULL,
+    "path" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "artistId" TEXT NOT NULL,
+
+    CONSTRAINT "File_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "BlacklistedToken" (
+    "id" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "blacklistedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "BlacklistedToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PasswordResetToken" (
+    "id" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "PasswordResetToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ArtistProfile_userId_key" ON "ArtistProfile"("userId");
+
+-- CreateIndex
+CREATE INDEX "ArtistProfile_artistName_idx" ON "ArtistProfile"("artistName");
+
+-- AddForeignKey
+ALTER TABLE "ArtistProfile" ADD CONSTRAINT "ArtistProfile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_artistId_fkey" FOREIGN KEY ("artistId") REFERENCES "ArtistProfile"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
