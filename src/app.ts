1bb398c27b55cfd7c13ac68060523e5828c36bb0
Yosinan <ymymym4634@gmail.com>
2024-07-15 19:59:24 +0300

    modified auth routes

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import status from "./routes/status";
-import userRouter from "./routes/userRoutes";
+import authRouter from "./routes/authRoutes";
 import fileRouter from "./routes/fileRoutes";
-import artistRouter from "./routes/artistRoutes";
+// import artistRouter from "./routes/artistRoutes";
 import errorHandler from "./middlewares/errorHandler";
 import setupSwagger from "./swagger/swagger";
 
@@ -16,8 +16,8 @@ app.use(cors());
 // Middleware to parse URL-encoded data 
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/user", userRouter);
-app.use("/artist", artistRouter);
+app.use("/auth", authRouter);
+// app.use("/artist", artistRouter);
 app.use("/", status);
 app.use("/files", fileRouter);
 
