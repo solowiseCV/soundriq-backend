28088cc780410ff57218c4b017bd616ce2e5afba
Yosinan <ymymym4634@gmail.com>
2024-07-15 19:54:42 +0300

    added: file routes, controllers, and services

--- src/routes/fileRoutes.ts
+++ src/routes/fileRoutes.ts
@@ -1,9 +1,23 @@
 import { Router } from "express";
+import { authenticate } from "../middlewares/authenticate";
 import FileController from "../controllers/fileController";
-import { upload } from "../middlewares/fileUpload";
+import { uploadFileMiddleware, upload } from "../middlewares/fileUpload";
 
 const router = Router();
 
-router.post("/upload", upload.single("file"), FileController.uploadFile);
+// upload a file/s
+router.post("/upload", authenticate, upload.array('file', 2), FileController.uploadFile);
+
+// View a file
+router.get('/files/:id', FileController.getFile);
+
+// View all files
+router.get('/files', FileController.getFiles);
+
+// Update a file
+router.put('/files/:id',upload.array('file', 1), FileController.updateFile);
+
+// Delete a file
+router.delete('/files/:id', FileController.deleteFile);
 
 export default router;
--- src/services/fileService.ts
+++ src/services/fileService.ts
@@ -1,29 +1,68 @@
 import prisma from "../config/database";
-// import mime from "mime-types";
+import mime from "mime-types";
 
 class FileService {
-  static async uploadFile(filename: string, path: string) {
+  static async uploadFile(artistProfileId: string, files: any) {
     try {
-      // const mimeType = mime.lookup(filename);
-      let file;
+      // Determine the file type
+      const mimeType = files.map((file: any) => mime.lookup(file.filename));
 
-      // if (!mimeType) {
-      //   throw new Error("Unable to determine file type");
-      // }
+      if (!mimeType) {
+        throw new Error("Unable to determine file type");
+      }
 
-      file = await prisma.file.create({
-        data: {
-          filename: filename,
-          path,
-          // type: mimeType,
-        },
-      });
-
-      return file;
-    } catch (error) {
-      console.error(error);
+      // Using $transaction to handle multiple file creation
+      const createdFiles = await prisma.$transaction(
+        files.map((file: any) =>
+          prisma.file.create({
+            data: {
+              filename: file.filename,
+              path: file.path,
+              artistId: artistProfileId,
+            },
+            include: { artist: true },
+          })
+        )
+      );
+      return createdFiles;
+    } catch (error: any) {
+      console.error(error.message);
       throw new Error("Failed to upload file");
     }
+  }
+
+  static async getFile(fileId: string) {
+    const file = await prisma.file.findUnique({
+      where: { id: fileId },
+      include: { artist: true },
+    });
+    return file;
+  }
+
+  static async getFiles(artistId: string) {
+    const files = await prisma.file.findMany({
+      where: { artistId },
+      include: { artist: true },
+    });
+    return files;
+  }
+
+  static async updateFile(fileId: string, files: any) {
+    const updatedFile = await prisma.file.update({
+      where: { id: fileId },
+      data: {
+        filename: files[0].filename,
+        path: files[0].path,
+      },
+    });
+    return updatedFile;
+  }
+
+  static async deleteFile(fileId: string) {
+    const file = await prisma.file.delete({
+      where: { id: fileId },
+    });
+    return file;
   }
 }
 
