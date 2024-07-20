276a659938c11e985ec91f766f58988fd62d3e3c
Yosinan <ymymym4634@gmail.com>
2024-07-20 23:57:11 +0300

    updated audio file uploading

--- dist/utils/cloudinary.js
+++ dist/utils/cloudinary.js
@@ -11,22 +11,25 @@ cloudinary_1.v2.config({
     api_secret: cloudinary_4.CLOUDINARY_API_SECRET,
     secure: true,
 });
-const uploadToCloudinary = async (filename, folder) => {
-    try {
-        const result = await cloudinary_1.v2.uploader.upload(filename, {
-            tags: "basic_sample",
-            folder: folder,
-        });
-        return result.secure_url;
-    }
-    catch (error) {
-        console.error(error);
-        throw new Error("Failed to upload file");
-    }
-};
 const uploadFilesToCloudinary = async (files) => {
     try {
-        const uploadPromises = files.map((file) => uploadToCloudinary(file.path, file.folder));
+        // check file type from path
+        // const uploadPromises = files.map((file) => {
+        //   if (file.path.endsWith(".mp3") || file.path.endsWith(".wav") || file.path.endsWith(".m4a")) {
+        //     return uploadAudioToCloudinary(file.path, file.folder);
+        //   } else {
+        //     return uploadImageToCloudinary(file.path, file.folder);
+        //   }
+        // }
+        // );
+        const uploadPromises = files.map((file) => {
+            if (file.type.startsWith("audio")) {
+                return uploadAudioToCloudinary(file.path, file.folder);
+            }
+            else {
+                return uploadImageToCloudinary(file.path, file.folder);
+            }
+        });
         const results = await Promise.all(uploadPromises);
         return results;
     }
@@ -36,3 +39,29 @@ const uploadFilesToCloudinary = async (files) => {
     }
 };
 exports.uploadFilesToCloudinary = uploadFilesToCloudinary;
+const uploadImageToCloudinary = async (path, folder) => {
+    try {
+        const result = await cloudinary_1.v2.uploader.upload(path, {
+            folder: folder,
+            resource_type: "image",
+        });
+        return result.secure_url;
+    }
+    catch (error) {
+        console.error(`Failed to upload image: ${error}`);
+        throw error;
+    }
+};
+const uploadAudioToCloudinary = async (path, folder) => {
+    try {
+        const result = await cloudinary_1.v2.uploader.upload(path, {
+            folder: folder,
+            resource_type: "video",
+        });
+        return result.secure_url;
+    }
+    catch (error) {
+        console.error(`Failed to upload audio: ${error}`);
+        throw error;
+    }
+};
--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -10,35 +10,68 @@ cloudinary.config({
   secure: true,
 });
 
-const uploadToCloudinary = async (
-  filename: string,
-  folder: string
-): Promise<string> => {
-  try {
-    const result = await cloudinary.uploader.upload(filename, {
-      tags: "basic_sample",
-      folder: folder,
-    });
+type FileType = "image" | "audio";
 
-    return result.secure_url;
-  } catch (error) {
-    console.error(error);
-    throw new Error("Failed to upload file");
-  }
-};
+interface File {
+  path: string;
+  folder: string;
+  type: FileType;
+}
 
 const uploadFilesToCloudinary = async (
-  files: { path: string; folder: string }[]
+  files: { path: string; folder: string, type: string }[]
 ) => {
   try {
-    const uploadPromises = files.map((file) =>
-      uploadToCloudinary(file.path, file.folder)
+   
+    // check file type from path
+    // const uploadPromises = files.map((file) => {
+    //   if (file.path.endsWith(".mp3") || file.path.endsWith(".wav") || file.path.endsWith(".m4a")) {
+    //     return uploadAudioToCloudinary(file.path, file.folder);
+    //   } else {
+    //     return uploadImageToCloudinary(file.path, file.folder);
+    //   }
+    // }
+    // );
+
+    const uploadPromises = files.map((file) => {
+      if (file.type.startsWith("audio")) {
+        return uploadAudioToCloudinary(file.path, file.folder);
+      } else {
+        return uploadImageToCloudinary(file.path, file.folder);
+      }
+    }
     );
     const results = await Promise.all(uploadPromises);
     return results;
   } catch (error) {
     console.error(error);
     throw new Error("Failed to upload files");
+  }
+};
+
+const uploadImageToCloudinary = async (path: string, folder: string) => {
+  try {
+    const result = await cloudinary.uploader.upload(path, {
+      folder: folder,
+      resource_type: "image",
+    });
+    return result.secure_url;
+  } catch (error) {
+    console.error(`Failed to upload image: ${error}`);
+    throw error;
+  }
+};
+
+const uploadAudioToCloudinary = async (path: string, folder: string) => {
+  try {
+    const result = await cloudinary.uploader.upload(path, {
+      folder: folder,
+      resource_type: "video",
+    });
+    return result.secure_url;
+  } catch (error) {
+    console.error(`Failed to upload audio: ${error}`);
+    throw error;
   }
 };
 
